2aaa76e66d7731ccf67cd8850ede5543e69fb31e
Emily Kuplic <emilykuplic@gmail.com>
2017-07-16 21:11:26 -0500

    favorite button with functional counter

--- server/public/scripts/photodata.js
+++ server/public/scripts/photodata.js
@@ -1,38 +1,38 @@
 //Array of images and data for html doc
  var photoArray = [
    {
-     image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 1',
+     image: '../images/roadtrip2017_1c5.jpg',
+     description: 'Happy to have made it to the top. Loved hiking and bouldering with Otto.',
      likes: 0,
      visible: true
    },
    {
      image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 2',
+     description: 'Even Otto was tired after making it to the top of Little Devils Tower.',
      likes: 0,
      visible: true
    },
    {
-     image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 3',
+     image: '../images/roadtrip2017_1d8.jpg',
+     description: 'Taking a much desired rest at the top of Little Devils Tower',
      likes: 0,
      visible: true
    },
    {
-     image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 4',
+     image: '../images/roadtrip2017_178.jpg',
+     description: 'The views were amazing when we were hiking in the Black Hills',
      likes: 0,
      visible: true
    },
    {
-     image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 5',
+     image: '../images/roadtrip2017_1c3.jpg',
+     description: 'Steven and Otto were bouldering like pros',
      likes: 0,
      visible: true
    },
    {
-     image: '../images/roadtrip2017_1ab.jpg',
-     description: 'photo 6',
+     image: '../images/roadtrip2017_203.jpg',
+     description: 'My two favorite guys posing when we started our 3 hr hike.',
      likes: 0,
      visible: true
    },
